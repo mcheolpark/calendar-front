dab097ed075b73258d2325be4648b21824af8f63
mcheolpark <mcheolpark@smilegate.com>
2019-08-11 23:02:14 +0900

    [MOD] 테스트 케이스 추가

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,47 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import {shallowMount} from '@vue/test-utils';
+import Day from '@/components/month/Day.vue';
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
-})
+describe('Day.vue', () => {
+    it('2019년 8월 14일 일정이 제대로 노출되는가?', () => {
+        const wrapper = shallowMount(Day, {
+            propsData: {
+                date: {
+                    day: 14,
+                    month: 8,
+                    year: 2019,
+                    currentMonth: true
+                },
+                schedule: [{
+                    id: 1,
+                    start: new Date(2019, 8, 14, 12),
+                    end: new Date(2019, 8, 14, 14),
+                    title: '일정등록 테스트'
+                }]
+            }
+        });
+
+        expect(wrapper.text()).toMatch('14 12~14 일정등록 테스트');
+    });
+    
+    it('지난달 일정(2019년 8월 14일)은 노출 안되어야 한다.', () => {
+        const wrapper = shallowMount(Day, {
+            propsData: {
+                date: {
+                    day: 14,
+                    month: 8,
+                    year: 2019,
+                    currentMonth: false
+                },
+                schedule: [{
+                    id: 1,
+                    start: new Date(2019, 8, 14, 12),
+                    end: new Date(2019, 8, 14, 14),
+                    title: '일정등록 테스트'
+                }]
+            }
+        });
+        //console.log(wrapper.vm);
+        console.log(wrapper.text());
+        expect(wrapper.text()).toMatch('');
+    });
+});
